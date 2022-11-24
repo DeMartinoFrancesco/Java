2cdac979a930567887f5013f04d807635537a562
De Martino <demartinofrancesco2002@gmail.com>
2022-11-24 16:20:38 +0100

    Aggiunta Programma Cicli

---
+++ ProgrammaCicli/src/NTTDATA/it/programmacicli/Main.java
@@ -0,0 +1,25 @@
+package NTTDATA.it.programmacicli;
+
+public class Main {
+    public static void main(String[] args) {
+        double c=1;
+        double n=0;
+
+
+        System.out.println("Ciclo While");
+
+        while(c<=16){
+            n = Math.pow(2,c);
+            System.out.println(n);
+            c++;
+        }
+        System.out.println("///////////////////////////////////////");
+
+        System.out.println("Ciclo For");
+
+        for(c=1;c<=16;c++){
+            n = Math.pow(2,c);
+            System.out.println(n);
+        }
+    }
+}
