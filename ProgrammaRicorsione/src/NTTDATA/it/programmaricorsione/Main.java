302abb0f379cf400f95b3c50edd6ee6e706bce08
De Martino <demartinofrancesco2002@gmail.com>
2022-11-24 17:54:01 +0100

    Aggiunta programma ricorsione V2.0

--- ProgrammaRicorsione/src/NTTDATA/it/programmaricorsione/Main.java
+++ ProgrammaRicorsione/src/NTTDATA/it/programmaricorsione/Main.java
@@ -2,16 +2,16 @@ package NTTDATA.it.programmaricorsione;
 
 public class Main {
     public static void main(String[] args) {
-        double n1=10,n2=15;
-
+        double n1=12,n2=15;
         System.out.println(aDouble(n1)+"/"+aDouble(n2));
     }
 
     private static double aDouble (double n){
-        if (n%2 == 0){
-            return 1+(n/2);
-        }else{
+
+        if (n%2 == 1){
             return 0;
+        }else{
+            return 1+aDouble(n/2);
         }
     }
 }
