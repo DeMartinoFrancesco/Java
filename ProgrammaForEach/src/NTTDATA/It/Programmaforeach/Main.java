deed64b163a7ccf76827e1f9788d798468a2c1dc
De Martino <demartinofrancesco2002@gmail.com>
2022-11-28 18:11:48 +0100

    Versione Finale

--- ProgrammaForEach/src/NTTDATA/It/Programmaforeach/Main.java
+++ ProgrammaForEach/src/NTTDATA/It/Programmaforeach/Main.java
@@ -8,13 +8,14 @@ public class Main {
         Scanner kb = new Scanner(System.in);
         int n;
         int nA[] = new int[10];
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < nA.length; i++){
             System.out.println("Inserisci il "+(i+1)+" numero");
             n = kb.nextInt();
             nA[i] = n;
         }
 
         Arrays.sort(nA);
+
         for(int a : nA){
             System.out.println(a);
         }
