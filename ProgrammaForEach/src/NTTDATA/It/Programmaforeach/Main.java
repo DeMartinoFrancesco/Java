7ee10a526a4b64255eb3c6ee79555d04bf5dd940
De Martino <demartinofrancesco2002@gmail.com>
2022-11-28 11:12:33 +0100

    Aggiunta Sort

---
+++ ProgrammaForEach/src/NTTDATA/It/Programmaforeach/Main.java
@@ -0,0 +1,22 @@
+package NTTDATA.It.Programmaforeach;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n;
+        int nA[] = new int[10];
+        for(int i = 0; i < 10; i++){
+            System.out.println("Inserisci il "+(i+1)+" numero");
+            n = kb.nextInt();
+            nA[i] = n;
+        }
+
+        Arrays.sort(nA);
+        for(int a : nA){
+            System.out.println(a);
+        }
+    }
+}
